170be66f4cb73867c1d03fbb7eac4fa6c2440207
nicolasGuinazu <nicolasguinazu1@gmail.com>
2022-10-21 14:19:57 -0300

    add/starting adding articles in home

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,37 @@
 import styles from '../styles/Home.module.css';
-import { Box } from "@chakra-ui/react";
-import ImageSlider from "./components/ImageSlider";
-import { SlideData } from "./components/SlideData";
+import { Box } from '@chakra-ui/react';
+import ImageSlider from './components/ImageSlider';
+import { SlideData } from './components/SlideData';
+import Contacto from './components/Contacto';
 
 export default function Home() {
   return (
     <>
       <main className={styles.main}>
-      <Box w="100%" p={4} color="white">
-      <ImageSlider slides={SlideData} />
-    </Box>
-        <div className={styles.containner}>
-     {/*      <div className={styles.wrapper}>
-            <img src="https://picsum.photos/200/100" />
-            <img src="https://picsum.photos/id/237/200/100" />
-            <img src="https://picsum.photos/id/236/200/100" />
-          </div> */}
-
-          <div>
-            <h1 className={styles.title}>77Mdq</h1>
-          </div>
+        <Box w="100%" p={4} color="white">
+          <ImageSlider slides={SlideData} show={true} auto={false} />
+        </Box>
+        <div>
+          <section>
+            <article className={styles.sectionArticle}>
+              <h2 className={styles.title}>Nostros</h2>
+              <Box w="100%" p={4} color="white">
+                <ImageSlider slides={SlideData} show={false} auto={true} />
+              </Box>
+            </article>
+            <article className={styles.sectionArticle}>
+              <h2 className={styles.title}>Unidades</h2>
+              <Box w="100%" p={4} color="white">
+                <ImageSlider slides={SlideData} show={false} auto={true} />
+              </Box>
+            </article>
+            <article className={styles.sectionArticleContact}>
+              <h2 className={styles.title}>Contacto</h2>
+              <div className={styles.contact}>
+                <Contacto />
+              </div>
+            </article>
+          </section>
         </div>
       </main>
     </>
