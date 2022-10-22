c41bbb54600b2ba03ce2ba527b2abb2214d814bd
nicolasGuinazu <nicolaguinazu1@gmail.com>
2022-10-22 01:22:14 -0300

    fix/change class title

--- pages/contacto.js
+++ pages/contacto.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css';
 export default function contacto() {
   return (
     <div className={styles.main}>
-      <h1 className={styles.title}>Contacto</h1>
+      <h1 className={styles.contactTitle}>Contacto</h1>
       <div className={styles.FormControl}>
         <Contacto />
       </div>
